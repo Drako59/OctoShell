055287de25b32d2ebae3f5e6343d46df5586c738
ayele <noam98038@gmail.com>
2026-02-01 20:49:25 +0200

    fix to the ping function

--- ping/ping.c
+++ ping/ping.c
@@ -25,11 +25,11 @@ int main(int argc, char** argv)
 	
 	 
 	char* test = "hello";
-	if (argc != 2)
+	/*if (argc != 2)
 	{
 		printf("ping: Enter valid number of parameters\n");
 		return 1;
-	}
+	}*/
 
 	/*CommandParsed command;
 	CommandParser_init(&command,argc,argv);*/
@@ -42,34 +42,16 @@ int main(int argc, char** argv)
 	if (strchr(strParametrs, 'a') || strchr(strParametrs, 'A')) {
 
 	}*/
-
-	const char* domain = argv[1];
-	struct addrinfo infoDNS;
-	ZeroMemory(&infoDNS, sizeof(infoDNS));
-
-	infoDNS.ai_family = AF_INET;
-	infoDNS.ai_socktype = SOCK_DGRAM;
+	struct sockaddr_in* addr;
 	WSADATA WSA;
 	DWORD startUpRet = WSAStartup(MAKEWORD(2, 2), &WSA);
 	if (startUpRet) {
 		printf("ping: wsa start up failed, ERROR: %d\n", startUpRet);
 		return 1;
 	}
-	struct addrinfo* resultDNS;
-	int ret = getaddrinfo(domain, NULL, &infoDNS, &resultDNS);
-	if (ret) {
-		printf("ping: Error occured while getting the IP, ERROR: %d\n", ret);
-		return 1;
-	}
-	struct sockaddr_in* addr = (struct sockaddr_int*)resultDNS->ai_addr;
-	
-	
-
-	char addrStr[INET6_ADDRSTRLEN];
-	inet_ntop(AF_INET, &(addr->sin_addr), addrStr, sizeof(addrStr));
-	printf("-----ping %s------\n",addrStr);
-
-
+	struct addrinfo infoDNS;
+	struct addrinfo* resultDNS = NULL;
+	const char* domain;
 	HANDLE hIcmpFile = IcmpCreateFile();
 
 	if (hIcmpFile == NULL)
@@ -77,45 +59,71 @@ int main(int argc, char** argv)
 		printf("ping: There was a problem in opening the ICMP file.");
 		return 1;
 	}
-	
+
 	IP_OPTION_INFORMATION options;
 	ZeroMemory(&options, sizeof(IP_OPTION_INFORMATION));
 	options.Ttl = 64;
 	char* replayBuffer[1024];
-	PICMP_ECHO_REPLY reply;
-	int success_counter = 0;
-	ULONG sum_time = 0;
-	for (int i = 0; i < 4; i++) {
-		IcmpSendEcho(hIcmpFile, addr->sin_addr.S_un.S_addr, test, sizeof(test) * strlen(test), &options, replayBuffer, sizeof(replayBuffer), 1000);
+	for (int j = 1; j < argc; j++) {
+		ZeroMemory(&infoDNS, sizeof(infoDNS));
 
-		reply = (PICMP_ECHO_REPLY)replayBuffer;
+		infoDNS.ai_family = AF_INET;
+		infoDNS.ai_socktype = SOCK_DGRAM;
 
-		if (reply->Status == IP_REQ_TIMED_OUT) {
-			printf("Ping request reached his timeout......\n");
+
+
+		domain = argv[j];
+
+		int ret = getaddrinfo(domain, NULL, &infoDNS, &resultDNS);
+		if (ret) {
+			printf("ping: Error occured while getting the IP, ERROR: %d\n", ret);
+			return 1;
 		}
-		else if (reply->Status == IP_TTL_EXPIRED_TRANSIT) {
-			printf("ping reached his hops limit and got thorwed away.\n");
+		addr = (struct sockaddr_int*)resultDNS->ai_addr;
+
+
+
+		char addrStr[INET6_ADDRSTRLEN];
+		inet_ntop(AF_INET, &(addr->sin_addr), addrStr, sizeof(addrStr));
+		printf("\n-----ping %s-->%s------\n", argv[j],addrStr);
+
+
+
+		PICMP_ECHO_REPLY reply;
+		int success_counter = 0;
+		ULONG sum_time = 0;
+		for (int i = 0; i < 4; i++) {
+			IcmpSendEcho(hIcmpFile, addr->sin_addr.S_un.S_addr, test, sizeof(test) * strlen(test), &options, replayBuffer, sizeof(replayBuffer), 1000);
+
+			reply = (PICMP_ECHO_REPLY)replayBuffer;
+
+			if (reply->Status == IP_REQ_TIMED_OUT) {
+				printf("Ping request reached his timeout......\n");
+			}
+			else if (reply->Status == IP_TTL_EXPIRED_TRANSIT) {
+				printf("ping reached his hops limit and got thorwed away.\n");
+
+			}
+			else if (reply->Status == IP_SUCCESS) {
+				success_counter++;
+				sum_time += reply->RoundTripTime;
+				char addrReplayStr[INET6_ADDRSTRLEN];
+				IN_ADDR replayAddr;
+				replayAddr.S_un.S_addr = reply->Address;
+				inet_ntop(AF_INET, &replayAddr, addrReplayStr, sizeof(addrReplayStr));
+
+				printf("REPLAY ADDR: %s-->%s RTT: %lums TTL: %u\n", argv[j] , addrReplayStr, reply->RoundTripTime, options.Ttl);
+			}
+			else {
+				printf("ping failed :(\n");
+			}
+			Sleep(1000);
 
 		}
-		else if (reply->Status == IP_SUCCESS) {
-			success_counter++;
-			sum_time += reply->RoundTripTime;
-			char addrReplayStr[INET6_ADDRSTRLEN];
-			IN_ADDR replayAddr;
-			replayAddr.S_un.S_addr = reply->Address;
-			inet_ntop(AF_INET, &replayAddr, addrReplayStr, sizeof(addrReplayStr));
 
-			printf("REPLAY ADDR: %s RTT: %lums TTL: %u\n", addrReplayStr, reply->RoundTripTime,options.Ttl);
+		if (success_counter > 1) {
+			printf("avg ping RTT: %.2fms\n", (double)sum_time / success_counter);
 		}
-		else {
-			printf("ping failed :(\n");
-		}
-		Sleep(1000);
-
-	}
-	
-	if (success_counter > 1) {
-		printf("avg ping RTT: %.2fms\n", (double)sum_time / success_counter);
 	}
 
 	/*
