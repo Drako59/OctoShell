f953ad41e1e7d1a6a7db75aae737442b32fed0cd
ayele <noam98038@gmail.com>
2026-02-18 22:47:15 +0200

    ofek told to stop so I push and going to his house to teach him a lesson.

--- grep/grep.c
+++ grep/grep.c
@@ -2,10 +2,11 @@
 //
 
 #include "CommandFuncs.h"
+#include <shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
 
 #define LINE_SIZE 1024
 #define BYTES_BUFFER_SIZE 4096
-
 //typedef struct Flags {
 //    BOOL TextFile;
 //    BOOL ByteFile;
@@ -13,7 +14,9 @@
 
 typedef enum  Flags {
     TextFile,
-    ByteFile,
+    BytesFile,
+    AllTextFiles,
+    AllBytesFiles,
     None
 } Flags;
 
@@ -41,17 +44,75 @@ BOOL CmpBytes(char* buffer, size_t bufferSize ,char* pattern) {
 //    flags->TextFile = FALSE;
 //
 //
-//}
+////}
+BOOL IsTextFile(LPSTR filename) {
+    LPSTR ext = PathFindExtensionA(filename);
 
+    if (ext && _stricmp(ext, ".txt") == 0) return TRUE;
+    return FALSE;
+}
+BOOL BytesGrep(Grep* grepInfo, int i) {
+    char buffer[BYTES_BUFFER_SIZE];
+    int bytesRead;
+    int patternLength = strlen(grepInfo->text);
+    if (grepInfo->files[i] != NULL) {
+        size_t numberOfBytes = BYTES_BUFFER_SIZE;
+
+        bytesRead = fread(buffer, sizeof(char), numberOfBytes, grepInfo->files[i]);
+        if (CmpBytes(buffer, bytesRead, grepInfo->text)) {
+            printf("\tFound in: %s\n", grepInfo->filesNames[i]);
+            return TRUE;
+        }
+        size_t tailSize = (patternLength - 1 < bytesRead) ? patternLength - 1 : bytesRead;
+        numberOfBytes = BYTES_BUFFER_SIZE - tailSize;
+
+        while ((bytesRead = fread(buffer + tailSize, sizeof(char), numberOfBytes, grepInfo->files[i])) > 0) {
+            if (CmpBytes(buffer, bytesRead + tailSize, grepInfo->text)) {
+                printf("\tgrep: Found in %s\n", grepInfo->filesNames[i]);
+                break;
+            }
+            memcpy(buffer, buffer + bytesRead - tailSize, tailSize);
+
+        }
+
+        printf("grep: Didn't found.\n");
+        return FALSE;
+    }
+    else {
+        printf("grep: didn't succeed opening %s\n", grepInfo->filesNames[i]);
+        return FALSE;
+    }
+}
+
+
+void TxtGrep(Grep* grepInfo, int i) {
+    char buffer[LINE_SIZE];
+    BOOL found;
+    if (grepInfo->files[i] != NULL) {
+        found = FALSE;
+
+        while (fgets(buffer, LINE_SIZE, grepInfo->files[i])) {
+            if (strstr(buffer, grepInfo->text)) {
+                printf("grep: %s", buffer);
+                found = TRUE;
+            }
+        }
+        if (!found) printf("Didn't found any matching in %s\n", grepInfo->filesNames[i]);
+    }
+    else {
+        printf("grep: didn't succeed opening %s\n", grepInfo->filesNames[i]);
+    }
+}
 
 int main(int argc, char** argv)
 {
     //DEBUG
-    /*argc = 4;
+    /*argc = 5;
     argv = (char**)malloc(sizeof(char*) * argc);
     argv[1] = "User";
     argv[2] = "C:\\Users\\ayele\\source\\repos\\Drako59\\OctoShell\\OctoShell\\tests_folder\\tests_files\\test4.txt";
-    argv[3] = "-a";*/
+    argv[3] = "-lll";
+    argv[4] = "C:\\Users\\ayele\\source\\repos\\Drako59\\OctoShell\\OctoShell\\tests_folder\\tests_files\\test_grep.txt";*/
     //
 
     CommandParsed command;
@@ -69,15 +130,17 @@ int main(int argc, char** argv)
     Flags Flags = None;
     //FlagsInit(&Flags);
 
+
     if (parametersString) {
         if (strchr(parametersString, 'a') || strchr(parametersString, 'A')) {
-            Flags = TextFile;
+            Flags = AllTextFiles;
         }
-        else if (strchr(parametersString, 'b') || strchr(parametersString, 'B')) {
-            Flags = ByteFile;
+        if (strchr(parametersString, 'b') || strchr(parametersString, 'B')) {
+            Flags = AllBytesFiles;
         }
     }
    
+    
     
     Grep grepInfo;
     grepInfo.text = command.argv[0];
@@ -93,84 +156,47 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    switch (Flags){
-        case TextFile:
-            {
-                for (int i = 1; i < grepInfo.filesNumber + 1; i++) {
-                    grepInfo.files[i - 1] = fopen(command.argv[i], "r");
-                    grepInfo.filesNames[i - 1] = command.argv[i];
+    for (int i = 1; i < grepInfo.filesNumber + 1; i++) {
 
-                }
-                char buffer[LINE_SIZE];
-                BOOL found; 
-
-                for (int i = 0; i < grepInfo.filesNumber; i++) {
-                    if (grepInfo.files[i] != NULL) {
-                        found = FALSE;
-
-                        while (fgets(buffer, LINE_SIZE, grepInfo.files[i])) {
-                            if (strstr(buffer, grepInfo.text)) {
-                                fputs(buffer, stdout);
-                                fputs("\n", stdout);
-                                found = TRUE;
-                            }
-                        }
-                        if(!found) printf("Didn't found any matching in %s\n", grepInfo.filesNames[i]);
-                    }
-                    else {
-                        printf("grep: didn't succeed opening %s\n", grepInfo.filesNames[i]);
-                    }
-                }
-                break;
+        if (Flags != AllBytesFiles && Flags != AllTextFiles ) {
+            if (IsTextFile(command.argv[i])) {
+                Flags = TextFile;
             }
-        case ByteFile:
-        {
-            for (int i = 1; i < grepInfo.filesNumber + 1; i++) {
-                grepInfo.files[i - 1] = fopen(command.argv[i], "rb");
-
-                grepInfo.filesNames[i-1] =  command.argv[i];
+            else {
+                Flags = BytesFile;
             }
-
-            char buffer[BYTES_BUFFER_SIZE];
-            int bytesRead;
-            int patternLength = strlen(grepInfo.text);
-            for (int i = 0; i < grepInfo.filesNumber; i++) {
-
-
-                if (grepInfo.files[i] != NULL) {
-                    size_t numberOfBytes = BYTES_BUFFER_SIZE;
-
-                    bytesRead = fread(buffer, sizeof(char), numberOfBytes, grepInfo.files[i]);
-                    if (CmpBytes(buffer, bytesRead, grepInfo.text)) {
-                        printf("\tFound in: %s\n", grepInfo.filesNames[i]);
-                        continue;
-                    }
-                    size_t tailSize = (patternLength - 1 < bytesRead) ? patternLength - 1 : bytesRead;
-                    numberOfBytes = BYTES_BUFFER_SIZE - tailSize;
-
-                    while ((bytesRead = fread(buffer + tailSize, sizeof(char), numberOfBytes, grepInfo.files[i])) > 0) {
-                        if (CmpBytes(buffer, bytesRead + tailSize, grepInfo.text)) {
-                            printf("\tFound in: %s\n", grepInfo.filesNames[i]);
-                            break;
-                        }
-                        memcpy(buffer, buffer + bytesRead - tailSize, tailSize);
-
-                    }
-
-                    if (bytesRead == 0) {
-                        printf("grep: Didn't found.\n");
-                    }
-                }
-                else {
-                    printf("grep: didn't succeed opening %s\n", grepInfo.filesNames[i]);
-                }
-            }
-            
-            
-
-            break;
         }
+        
+        switch (Flags){
+                case TextFile: case AllTextFiles:
+                    {
+                        grepInfo.files[i - 1] = fopen(command.argv[i], "r");
+                        grepInfo.filesNames[i - 1] = command.argv[i];
+
+                        
+                        TxtGrep(&grepInfo,i - 1);
+                        
+                        break;
+                    }
+                case BytesFile: case AllBytesFiles:
+                {
+                    grepInfo.files[i - 1] = fopen(command.argv[i], "rb");
+
+                    grepInfo.filesNames[i-1] =  command.argv[i];
+
+                    
+                    BytesGrep(&grepInfo, i - 1);
+
+                    
+            
+            
+
+                    break;
+                }
+        }
+        fputs("\n",stdout);
     }
+
 
 
 
